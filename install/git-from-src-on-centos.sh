a1166f4eaba9874edf982e096712a088b9e70c74
yukihane <yukihane.feather@gmail.com>
2018-12-18 10:17:18 +0900

    git install スクリプト

---
+++ install/git-from-src-on-centos.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -eux
+
+git_ver=git-2.20.1
+# Dockerイメージにはsudoが無い、かつrootでセットアップしている前提なので
+sudo=$(command -v sudo)
+
+${sudo} yum -y install gcc autoconf zlib-devel curl-devel expat-devel gettext-devel openssl-devel perl-devel
+
+pushd /tmp
+curl -L -o git-src.tar.xz https://mirrors.edge.kernel.org/pub/software/scm/git/${git_ver}.tar.xz
+tar xf git-src.tar.xz
+cd ${git_ver}
+
+make configure
+./configure --prefix=/usr/local
+make
+
+${sudo} make install
+
+cd ..
+rm -rf git-src.tar.xz ${git_ver}
+unset git_ver
+unset sudo
+popd
