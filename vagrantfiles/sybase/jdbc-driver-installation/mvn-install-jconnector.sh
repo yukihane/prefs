a82a9cbafd49b70f043c3a364298341f7c70fb10
yukihane <yukihane.feather@gmail.com>
2019-02-02 01:31:46 +0000

    デフォルトインストール時の場所をコメントで示唆

--- vagrantfiles/sybase/jdbc-driver-installation/mvn-install-jconnector.sh
+++ vagrantfiles/sybase/jdbc-driver-installation/mvn-install-jconnector.sh
@@ -3,6 +3,7 @@
 set -eux
 
 dir=$(dirname $BASH_SOURCE)
+# dir=/opt/sap/jConnect-16_0/classes
 
 mvn install:install-file -Dfile=$dir/jconn4.jar -DgroupId=com.sybase \
  -DartifactId=jconnect -Dversion=16.0.b27403 -Dpackaging=jar
